3f92399b3df03124b29b6bd58ad100ee318da931
haifaarouri <haifa.arouri@esprit.tn>
2023-02-22 16:14:47 +0100

    functional component

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,43 +1,66 @@
 import React from "react";
 import { Card, Button } from 'react-bootstrap'
 
-export default class Product extends React.Component {
+export default function Products(props) {
 
-    constructor(props) {
-        super(props);
-        this.state = props.prod
-        //console.log(props.prod)
-        this.addLikes = this.addLikes.bind(this);
-        //this.state = { ...props};
-
+    function addLikes(){
+        props.prod.like ++
     }
 
-    addLikes = () =>
-        //this.setState(oldState=>{(product:{...oldState.product, like: oldState.product.like+1})}
-        // ou bien 
-        // e.preventDefault();
-        // this.setState((oldState) => ({
-        //   produit: { ...oldState.produit, like: oldState.produit.like + 1 },
-        // }));
-        this.setState((oldState) => ({
-            like: oldState.like + 1
-        }))
-
-    render() {
-        return <Card className="flex">
-            <Card.Header>
-                <Card.Img src={require('../assets/images/' + this.state.img)}></Card.Img>
-            </Card.Header>
-            <Card.Body style={{ textAlign: "center" }}>
-                <Card.Title>{this.state.name}</Card.Title>
-                <Card.Text>{this.state.description}</Card.Text>
-                <Card.Text>Price : {this.state.price} DT</Card.Text>
-                <Card.Text>Quantity : {this.state.quantity}</Card.Text>
-                <Card.Text>Likes : {this.state.like}</Card.Text>
-            </Card.Body>
-            <Card.Footer style={{ backgroundColor: "white" }}>
-                <Button variant="primary" onClick={this.addLikes}>Likes</Button>
-                <Button variant="info" style={{float: "right", backGroundColor: "cyan"}} disabled={this.props.prod.quantity===0} onClick={this.props.handleBuy}>Buy</Button>            </Card.Footer>
-        </Card>;
-    }
+    return <Card className="flex">
+    <Card.Header>
+        <Card.Img src={require('../assets/images/' + props.prod.img)}></Card.Img>
+    </Card.Header>
+    <Card.Body style={{ textAlign: "center" }}>
+        <Card.Title>{props.prod.name}</Card.Title>
+        <Card.Text>Price : {props.prod.price} DT</Card.Text>
+        <Card.Text>Quantity : {props.prod.quantity}</Card.Text>
+        <Card.Text>Likes : {props.prod.like}</Card.Text>
+    </Card.Body>
+    <Card.Footer style={{ backgroundColor: "white" }}>
+        <Button variant="primary" onClick={addLikes}>Likes</Button>
+        <Button variant="info" style={{float: "right", backGroundColor: "cyan"}} disabled={props.prod.quantity===0} onClick={props.handleBuy}>Buy</Button>            
+    </Card.Footer>
+    </Card>;
 }
+
+// export default class Product extends React.Component {
+
+//     constructor(props) {
+//         super(props);
+//         this.state = props.prod
+//         //console.log(props.prod)
+//         this.addLikes = this.addLikes.bind(this);
+//         //this.state = { ...props};
+
+//     }
+
+//     addLikes = () =>
+//         //this.setState(oldState=>{(product:{...oldState.product, like: oldState.product.like+1})}
+//         // ou bien 
+//         // e.preventDefault();
+//         // this.setState((oldState) => ({
+//         //   produit: { ...oldState.produit, like: oldState.produit.like + 1 },
+//         // }));
+//         this.setState((oldState) => ({
+//             like: oldState.like + 1
+//         }))
+
+//     render() {
+//         return <Card className="flex">
+//             <Card.Header>
+//                 <Card.Img src={require('../assets/images/' + this.state.img)}></Card.Img>
+//             </Card.Header>
+//             <Card.Body style={{ textAlign: "center" }}>
+//                 <Card.Title>{this.state.name}</Card.Title>
+//                 <Card.Text>{this.state.description}</Card.Text>
+//                 <Card.Text>Price : {this.state.price} DT</Card.Text>
+//                 <Card.Text>Quantity : {this.state.quantity}</Card.Text>
+//                 <Card.Text>Likes : {this.state.like}</Card.Text>
+//             </Card.Body>
+//             <Card.Footer style={{ backgroundColor: "white" }}>
+//                 <Button variant="primary" onClick={this.addLikes}>Likes</Button>
+//                 <Button variant="info" style={{float: "right", backGroundColor: "cyan"}} disabled={this.props.prod.quantity===0} onClick={this.props.handleBuy}>Buy</Button>            </Card.Footer>
+//         </Card>;
+//     }
+// }
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Product from './Product'
 import AllProducts from '../products.json'
 import Container from 'react-bootstrap/Container';
@@ -6,49 +6,91 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Alert } from 'react-bootstrap';
 
-export default class Products extends React.Component {
+export default function Products() {
+    const [{alertbienvenue}, setAlert] = useState({alertbienvenue:true})
+    const [alertBuy, setAlertBuy] = useState(false)
 
-    constructor(props) {
-        super(props);
-        console.log("Products component");
-        this.listProducts = AllProducts;
-        this.state = {
-            prodList: this.listProducts,
-        };
-        this.state = {
-            ...props, alertbienvenue: true, alertBuy: false
-        }
-    }
-    componentDidMount() {
+    useEffect(()=> {
         setTimeout(() => {
-            this.setState({ alertbienvenue: false });
-        }, 3000);
-    }
-    buy = () => {
-        if (!this.state.alertBuy) {
-            this.setState({ alertBuy: !this.state.alertBuy });
-            setTimeout(() => {
-                this.setState({ alertBuy: !this.state.alertBuy });
-            }, 2000);
+            setAlert({alertbienvenue: false})
+            }, 3000);
+    })
+
+    function buy (product){
+        if (!alertBuy) {
+            setAlertBuy(  true );
+            product.quantity --;
         }
     }
-    render() {
-        return <div>
-            {this.state.alertbienvenue && (<Alert variant="success">
-                <Alert.Heading>Hey, Welcome to Our Shop <b>MyStrore</b></Alert.Heading>
-                Thank you for choosing our strore, we hope you enjoy your shopping experience!
-                <hr />
-            </Alert>)}
-            <Container>
-                <Row>
-                    <Col className="d-flex">
-                        {this.listProducts.map((p, index) => <Product key={index} prod={p} handleBuy={this.buy}></Product>)}
-                    </Col>
-                </Row>
-            </Container>
-            {this.state.alertBuy && (<Alert variant="info">
-                You Bought an item
-            </Alert>)}
-        </div>
-    }
+
+    useEffect(()=> {
+        setTimeout(() => {
+            setAlertBuy(false)
+            }, 2000);
+    }, [alertBuy])
+
+    return <div>
+        {alertbienvenue && (<Alert variant="success">
+        <Alert.Heading>Hey, Welcome to Our Shop <b>MyStore</b></Alert.Heading>
+        Thank you for choosing our strore, we hope you enjoy your shopping experience!
+        <hr />
+        </Alert>)}
+    <Container>
+        <Row>
+            <Col className="d-flex">
+            {AllProducts.map((p, index) => <Product key={index} prod={p} handleBuy={buy}></Product>)}
+            </Col>
+        </Row>
+        {alertBuy && (<Alert variant="info">
+            You Bought an item
+        </Alert>)}
+    </Container>
+    </div>;
 }
+
+// export default class Products extends React.Component {
+
+//     constructor(props) {
+//         super(props);
+//         console.log("Products component");
+//         this.listProducts = AllProducts;
+//         this.state = {
+//             prodList: this.listProducts,
+//         };
+//         this.state = {
+//             ...props, alertbienvenue: true, alertBuy: false
+//         }
+//     }
+//     componentDidMount() {
+//         setTimeout(() => {
+//             this.setState({ alertbienvenue: false });
+//         }, 3000);
+//     }
+//     buy = () => {
+//         if (!this.state.alertBuy) {
+//             this.setState({ alertBuy: !this.state.alertBuy });
+//             setTimeout(() => {
+//                 this.setState({ alertBuy: !this.state.alertBuy });
+//             }, 2000);
+//         }
+//     }
+//     render() {
+//         return <div>
+//             {this.state.alertbienvenue && (<Alert variant="success">
+//                 <Alert.Heading>Hey, Welcome to Our Shop <b>MyStrore</b></Alert.Heading>
+//                 Thank you for choosing our strore, we hope you enjoy your shopping experience!
+//                 <hr />
+//             </Alert>)}
+//             <Container>
+//                 <Row>
+//                     <Col className="d-flex">
+//                         {this.listProducts.map((p, index) => <Product key={index} prod={p} handleBuy={this.buy}></Product>)}
+//                     </Col>
+//                 </Row>
+//             </Container>
+//             {this.state.alertBuy && (<Alert variant="info">
+//                 You Bought an item
+//             </Alert>)}
+//         </div>
+//     }
+// }
