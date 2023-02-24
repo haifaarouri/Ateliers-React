c2dfd16a4530f04317e01a415beed871e081d40a
haifaarouri <haifa.arouri@esprit.tn>
2023-02-24 10:33:43 +0100

    adding lazy loading

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import { Route, Routes } from 'react-router-dom';
-import React from 'react';
-import NotFound from './Components/NotFound';
-import Products from './Components/Products'
-import NavigationBar from './Components/NavigationBar';
-import ProductDetails from './Components/ProductDetails'
+import { Route, Routes } from 'react-router-dom'
+import React, { Suspense } from 'react'
+import NavigationBar from './Components/NavigationBar'
 
 function App() {
+
+  const Products = React.lazy(()=>import('./Components/Products'))
+  const ProductDetails = React.lazy(()=>import('./Components/ProductDetails'))
+  const NotFound = React.lazy(()=>import('./Components/NotFound'))
+
   return (
     <>
     <NavigationBar></NavigationBar>
-    <Routes>
-      <Route path='/' element={<Products/>}/>
-      <Route path='/products' element={<Products/>}/>
-      <Route path='/details/:name' element={<ProductDetails/>}/>
-      <Route path='*' element={<NotFound/>}/>
-    </Routes>
+    <Suspense fallback={<p>Loading ...</p>}>
+      <Routes>
+        <Route path='/' element={<Products/>}/>
+        <Route path='/products' element={<Products/>}/>
+        <Route path='/details/:name' element={<ProductDetails/>}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Routes>
+    </Suspense>
     </>
   );
 }
--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -5,17 +5,25 @@ import { Container } from 'react-bootstrap';
 
 export default function NavigationBar() {
     return(
-        <Navbar bg="light" expand="lg">
+        // <Navbar bg="light" expand="lg">
+        //     <Container>
+        //         <Nav className="me-auto">
+        //             <Nav.Link>
+        //                 My Store
+        //             </Nav.Link>
+        //             <Nav.Link>
+        //                 <NavLink 
+        //                 to="/products"
+        //                 style={({ isActive }) => ({textDecoration: isActive ? "underline" : "none"})}>Products</NavLink>
+        //             </Nav.Link>
+        //         </Nav>
+        //     </Container>
+        // </Navbar>);
+        <Navbar bg="light" variant="light">
             <Container>
+                <Navbar.Brand to="/products">MyStore</Navbar.Brand>
                 <Nav className="me-auto">
-                    <Nav.Link>
-                        My Store
-                    </Nav.Link>
-                    <Nav.Link>
-                        <NavLink 
-                        to="/products"
-                        style={({ isActive }) => ({textDecoration: isActive ? "underline" : "none"})}>Products</NavLink>
-                    </Nav.Link>
+                    <Nav.Link as={NavLink} to="/products" style={({isActive})=>({textDecoration:isActive&&'underline'})}>Products</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>);
